d7805ea1c22c91275470a0d1bdf8a26373752a01
Manojkumar-13 <manojdmice@gmail.com>
2022-05-16 13:31:05 +0530

    css part completed

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const balance =  document.getElementById('balance');
+const moneyPlus =  document.getElementById('money-plus');
+const moneyMinus =  document.getElementById('money-minus');
+const balance =  document.getElementById('balance')
+const balance =  document.getElementById('balance')
